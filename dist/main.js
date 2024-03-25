44b5d0caeba30e9bbfe812264603d5aa81dd5926
Gustavo Dias <gustavogdcosta@hotmail.com>
2024-03-25 09:12:04 -0300

    ajustando comentários

--- dist/main.js
+++ dist/main.js
@@ -1,37 +1,23 @@
 "use strict";
 
 function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
 function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
 function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
-var Aluno = /*#__PURE__*/function () {
-  function Aluno(nomeDoAluno, notaDoAluno) {
-    _classCallCheck(this, Aluno);
-    this.nome = nomeDoAluno;
-    this.nota = notaDoAluno;
-  }
-  return _createClass(Aluno, [{
-    key: "aprovadoReprovad",
-    value: function aprovadoReprovad() {
-      if (this.nota >= 6) {
-        console.log("".concat(this.nome, " est\xE1 aprovado(a) com nota ").concat(this.nota));
-      } else {
-        console.log("Infelizmente, ".concat(this.nome, " foi reprovado(a) com nota ").concat(this.nota));
-      }
-    }
-  }]);
-}();
-var Aluno1 = new Aluno('Aluno1', 8);
-var Aluno2 = new Aluno('Aluno2', 2);
-var Aluno3 = new Aluno('Aluno3', 5);
-var Aluno4 = new Aluno('Aluno4', 7);
-console.log(Aluno1);
-Aluno1.aprovadoReprovad();
-console.log(Aluno2);
-Aluno2.aprovadoReprovad();
-console.log(Aluno3);
-Aluno3.aprovadoReprovad();
-console.log(Aluno4);
-Aluno4.aprovadoReprovad();
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+var Aluno = /*#__PURE__*/_createClass(function Aluno(nomeDoAluno, notaDoAluno) {
+  _classCallCheck(this, Aluno);
+  this.nome = nomeDoAluno;
+  this.nota = notaDoAluno;
+});
+var alunos = ['Aluno 1', 'Aluno 2', 'Aluno 3', 'Aluno 4'];
+var notas = [7, 5, 6, 2];
+var alunosNotas = alunos.map(function (nomeDoAluno, Index) {
+  return new Aluno(nomeDoAluno, notas[Index]);
+});
+function aprovados(aluno) {
+  return aluno.nota >= 6;
+}
+var alunosAprovados = alunosNotas.filter(aprovados);
+console.log('Os alunos aprovados são: ', alunosAprovados);
--- src/main.js
+++ src/main.js
@@ -3,28 +3,19 @@ class Aluno{
         this.nome = nomeDoAluno;
         this.nota = notaDoAluno;
     }
-
-    aprovadoReprovad(){
-        if (this.nota >= 6) {console.log(`${this.nome} está aprovado(a) com nota ${this.nota}`)}
-        else {console.log(`Infelizmente, ${this.nome} foi reprovado(a) com nota ${this.nota}`)}
-    }
 }
 
-const Aluno1 = new Aluno('Aluno1', 8);
-const Aluno2 = new Aluno('Aluno2', 2);
-const Aluno3 = new Aluno('Aluno3', 5);
-const Aluno4 = new Aluno('Aluno4', 7);
+var alunos = ['Aluno 1', 'Aluno 2', 'Aluno 3', 'Aluno 4'];
 
+var notas = [7,5,6,2];
 
-console.log(Aluno1);
-Aluno1.aprovadoReprovad();
-console.log(Aluno2);
-Aluno2.aprovadoReprovad();
-console.log(Aluno3);
-Aluno3.aprovadoReprovad();
-console.log(Aluno4);
-Aluno4.aprovadoReprovad();
+var alunosNotas = alunos.map(function(nomeDoAluno, Index){
+    return new Aluno (nomeDoAluno, notas[Index])
+})
 
+function aprovados(aluno) {
+    return aluno.nota >= 6;
+} 
 
-
-
+const alunosAprovados = alunosNotas.filter(aprovados);
+console.log('Os alunos aprovados são: ', alunosAprovados);
